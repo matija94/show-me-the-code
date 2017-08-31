571dd5c84d85168e5f380520de3962ab2ffe1c76
matija <matija.lukovic@yoterra.com>
2017-08-31 22:48:38 +0200

    traversal tree algorithms

--- Algorithms/trees/base.py
+++ Algorithms/trees/base.py
@@ -1,3 +1,4 @@
+from linked_lists.linked_queue import LinkedQueue
 class Tree:
     ''' base abstract class for all implementations of trees '''
     class Position:
@@ -58,6 +59,57 @@ class Tree:
             p = self.root()
         return self._height(p)
         
+    def preorder(self):
+        ''' Rather than visiting nodes this function just yields positions in the tree and lets the caller do the 'visiting'.
+            This traversal goes into depth. It is one of three types of depth-first-search traversal algorithm
+            Preorder goes in depth in such way that it yields root of the subtree before visiting children
+        '''
+        if not self.is_empty():
+            for c in self._subtree_preorder(self.root()):
+                yield c
+    
+    def _subtree_preorder(self,p):
+        yield p
+        for c in self.children(p):
+            for other in self._subtree_preorder(c):
+                yield other
+    
+    def postorder(self):
+        ''' Rather than visiting nodes this function just yields positions in the tree and lets the caller do the 'visiting'.
+            This traversal goes into depth. It is one of three types of depth-first-search traversal algorithm
+            Postorder goes in depth in such way that it yields children of root in subtree first
+        '''
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):
+                yield p
+    
+    def breadthfirst(self):
+        ''' Rather than visiting nodes this function just yields positions in the tree and lets the caller do the 'visiting'.
+            This traversal goes into width. It is famous tree traversal algorithm
+            BFS goes in width in such way that it yields all elements from left to right in each tree-level starting from the root of the tree
+        '''
+        if not self.is_empty():
+            que = LinkedQueue()
+            que.enqueue(self.root())
+            while not que.is_empty():
+                p = que.dequeue()
+                yield p
+                for c in self.children(p):
+                    que.enqueue(c)
+    
+    def _subtree_postorder(self,p):
+        for c in self.children(p):
+            for other in self._subtree_postorder(c):
+                yield other
+        yield p
+    
+    def positions(self, impl=Tree.preorder):
+        ''' Yields all positions from the tree
+            Implementation can be passed as reference to one of the tree traversal algorithms in order to generate elements in such manner
+            Default implementation is preorder tree traversal
+        '''
+        return self.impl()
+        
 class BinaryTree(Tree):
     ''' base abstract class representing binary tree structure '''
     
@@ -85,5 +137,37 @@ class BinaryTree(Tree):
             yield self.left(p)
         if self.right(p) is not None:
             yield self.right(p)
-            
     
+    def inorder(self):
+        ''' Rather than visiting nodes this function just yields positions in the tree and lets the caller do the 'visiting'.
+            This traversal goes into depth. It is one of three types of depth-first-search traversal algorithm
+            Inorder goes in depth in such way that it yields elements from most left of the tree to the most right of the tree
+            I.e 0
+               / \
+             1    2
+            / \  / \
+           3  4  5  6
+           
+           produces : [3,1,4,0,5,2,6]
+           
+           Inorder is only used for binary trees(decision trees).
+           Good example of inorder algorithm usage is computing arithmetic expression(where operands are saved as external nodes and operations are saved as internal nodes)
+        '''
+        if not self.is_empty():
+            for p in self._subtree_inorder(p):
+                yield p
+            
+    def _subtree_inorder(self,p):
+        if self.left(p) is not None:
+            for other in self._subtree_inorder(self.left(p)):
+                yield other
+        yield p
+        if self.right(p) is not None:
+            for other in self._subtree_inorder(self.right(p)):
+                yield other
+            
+    def positions(self, impl=BinaryTree.inorder):
+        ''' Uses inorder traversal algorithm
+            Check inorder function documentation for more information about algorithm
+        '''
+        return self.impl()
