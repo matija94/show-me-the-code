53ebe3a0369fd8249bcbe5469fa3df952aa5b0f4
matija <matija.lukovic@yoterra.com>
2017-10-08 21:44:07 +0200

    sorted map

--- Algorithms/maps/concrete.py
+++ Algorithms/maps/concrete.py
@@ -107,4 +107,118 @@ class ProbeHashMap(HashMapBase):
             if not self._is_available(i):
                 yield self._table[i]._key
                 
-                
+
+class SortedTableMap(MapBase):
+    
+    ''' map representation using sorted table '''
+    
+    ''' ### private methods ### '''
+    def _find_index(self,k,low,high):
+        '''
+        Return index of the leftmost item with key greater than or equal to k.
+        
+        Return high + 1 if no such item qualifies.
+        That is, j will be returned such that:
+        all items of slice table[low:j] have key < k
+        all items of slice table[j:high+1] have key >= k
+        '''
+        if high < low:
+            return high+1
+        mid = (low + high) // 2
+        if k == self._table[mid]._key:
+            return mid
+        elif k < self._table[mid]._key:
+            self._find_index(k, low, mid-1)
+        else:
+            self._find_index(k, mid+1, high)
+    
+    ''' ### public methods ### '''
+    def __init__(self):
+        self._table = []
+        
+    def __len__(self):
+        return len(self._table)
+    
+    def __getitem__(self, key):
+        j = self._find_index(key, 0, len(self._table)-1)
+        if j == len(self._table) or self._table[j]._key != key:
+            raise KeyError('Key Error: ' + repr(key))
+        return self._table[j]._value
+    
+    def __setitem__(self, key, value):
+        j = self._find_index(key, 0, len(self._table)-1)
+        if j == len(self._table) or self._table[j]._key != key:
+            self._table.insert(j, self._Item(key,value))
+        else:
+            self._table[j]._value = value
+            
+    def __delitem__(self, key):
+        j = self._find_index(key, 0, len(self._table)-1)
+        if j == len(self._table) or self._table[j]._key != key:
+            raise KeyError('Key Error: ' + repr(key))
+        self._table.pop(j)
+        
+    def __iter__(self):
+        for item in self._table:
+            yield item._key
+            
+    def __reversed__(self):
+        for item in reversed(self._table):
+            yield item._key
+            
+    def find_min(self):
+        if len(self._table) > 0:
+            return (self._table[0]._key, self._table[0]._value)
+        else:
+            return None
+    
+    def find_max(self):
+        if len(self._table>0):
+            return (self._self._table[-1]._key, self._table[-1]._value)
+        else:
+            return None
+    
+    def find_ge(self,k):
+        '''
+        Find least key grater than or equal to k
+        Returns (key, value) pair
+        '''
+        j = self._find_index(k, 0, len(self._table)-1)
+        if j >= len(self._table):
+            return None
+        return (self._table[j]._key, self._table[j]._value)
+    
+    def find_lt(self,k):
+        '''
+        Finds greatest key strictly less than k
+        Returns (key,value) pair
+        '''
+        j = self._find_index(k, 0, len(self._table)-1)
+        if j > 0:
+            return (self._table[j-1]._key, self._table[j-1]._value)
+        else:
+            return None
+    
+    def find_gt(self, k):
+        '''Return (key,value) pair with least key strictly greater than k.'''
+        j = self._find_index(k, 0, len(self._table)-1)
+        if j >= len(self._table):
+            return None
+        elif k == self._table[j]._key and j+1 < len(self._table):
+            return (self._table[j+1]._key, self._table[j+1]._value)
+        else:
+            return (self._table[j]._key, self._table[j]._value)
+        
+    def find_range(self, start, stop):
+        '''
+        Iterate all (key,value) pairs such that start <= key < stop.
+        If start is None, iteration begins with minimum key of map.
+        If stop is None, iteration continues through the maximum key of map.
+        '''
+        if start is None:
+            j = 0
+        else:
+            j = self._find_index(start, 0, len(self._table)-1)
+        while j < len(self._table) and (stop is None or self._table[j]._key < stop):
+            yield (self._table[j]._key, self._table[j]._value)
+            j+=1
